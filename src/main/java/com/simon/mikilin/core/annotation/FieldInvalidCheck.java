7638e78e3619adea06da2e205178d13182e6d23b
simonalong <simonalongs@outlook.com>
2019-04-10 08:50:43 +0800

    add 增加1.3.0相关的功能

--- src/main/java/com/simon/mikilin/core/annotation/FieldInvalidCheck.java
+++ src/main/java/com/simon/mikilin/core/annotation/FieldInvalidCheck.java
@@ -28,6 +28,46 @@ public @interface FieldInvalidCheck {
     FieldEnum type() default FieldEnum.DEFAULT;
 
     /**
+     * 枚举类型的判断
+     *
+     * @return 如果位于枚举类型内部，则拦截，否则认为核查通过
+     */
+    Class[] enumType() default {};
+
+    /**
+     * 数据范围的判断
+     *
+     * @return 如果类型是数值类型，且位于范围之内，则拦截，当前支持的核查功能：[a,b]，[a,b)，(a,b]，(a,b)
+     */
+    String range() default "";
+
+    /**
+     * 数据条件的判断
+     *
+     * 根据Java的运算符构造出来对应的条件表达式来进行判断，而其中的数据不仅可以和相关的数据做条件判断，还可和当前修饰的类的其他数据进行判断，
+     * 其中当前类用#root表示，比如举例如下，对象中的一个属性小于另外一个属性
+     * {@code
+     * class ToCheckClass{
+     *
+     *      @FieldInvalidCheck(condition="#root.ratioA > #root.ratioB")
+     *      private String ratioA;
+     *      private String ratioB;
+     * }
+     * }
+     * 如上只有在属性ratioA是大于ratioB的时候核查才会拦截
+     * </p>
+     *
+     * @return 用于数据字段之间的条件表达式（即条件结果为true还是false），当前条件支持Java的所有运算符：
+     * 算术运算符：+、-、*、/、％、++、--
+     * 关系运算符：==、!=、>、<、>=、<=
+     * 位运算符：＆、|、^、~、<<、>>、>>>
+     * 逻辑运算符：&&、||、!
+     * 赋值运算符：=、+=、-=、*=、/=、(%)=、<<=、>>=、&=、^=、|=
+     * 其他运算符：条件运算符（?:）、instanceof运算符
+     */
+    String condition() default "";
+
+    /**
      * 禁用的值对应的正则表达式
      * @return 对应的正则表达式
      */
--- src/main/java/com/simon/mikilin/core/annotation/FieldValidCheck.java
+++ src/main/java/com/simon/mikilin/core/annotation/FieldValidCheck.java
@@ -27,20 +27,60 @@ public @interface FieldValidCheck {
     FieldEnum type() default FieldEnum.DEFAULT;
 
     /**
+     * 枚举类型的判断
+     *
+     * @return 如果位于枚举类型内部，则通过，否则核查失败
+     */
+    Class[] enumType() default {};
+
+    /**
+     * 数据范围的判断
+     *
+     * @return 如果是数值类型，且位于范围之内，则核查成功，当前支持的核查功能：[a,b]，[a,b)，(a,b]，(a,b)
+     */
+    String range() default "";
+
+    /**
+     * 数据条件的判断
+     *
+     * 根据Java的运算符构造出来对应的条件表达式来进行判断，而其中的数据不仅可以和相关的数据做条件判断，还可和当前修饰的类的其他数据进行判断，
+     * 其中当前类用#root表示，比如举例如下，对象中的一个属性小于另外一个属性
+     * {@code
+     * class ToCheckClass{
+     *
+     *      @FieldValidCheck(condition="#root.ratioA > #root.ratioB")
+     *      private String ratioA;
+     *      private String ratioB;
+     * }
+     * }
+     * 如上只有在属性ratioA是大于ratioB的时候核查才会成功
+     * </p>
+     *
+     * @return 用于数据字段之间的条件表达式（即条件结果为true还是false），当前条件支持Java的所有运算符：
+     * 算术运算符：+、-、*、/、％、++、--
+     * 关系运算符：==、!=、>、<、>=、<=
+     * 位运算符：＆、|、^、~、<<、>>、>>>
+     * 逻辑运算符：&&、||、!
+     * 赋值运算符：=、+=、-=、*=、/=、(%)=、<<=、>>=、&=、^=、|=
+     * 其他运算符：条件运算符（?:）、instanceof运算符
+     */
+    String condition() default "";
+
+    /**
      * 可用的值对应的正则表达式
      * @return 对应的正则表达式
      */
     String regex() default "";
 
     /**
-     * 内部的判断的调用
+     * 内部的判断的回调
      *
      * @return 调用的核查的类和函数对应的表达式，比如："com.xxx.AEntity#isValid"，其中#后面是方法，方法返回boolean或者包装类，入参为当前Field对应的类型或者子类
      */
     String judge() default "";
 
     /**
-     * 是否不可用
+     * 是否启用核查
      * @return true：禁用核查，false：启用核查
      */
     boolean disable() default false;
