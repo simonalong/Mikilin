d86f0bab3ed75eca04e1f4eac9f0199763f58790
shizi <zhouzy@isyscore.com>
2021-07-20 21:41:19 +0800

    add 添加参数的空的用例

--- src/test/java/com/simonalong/mikilin/throwable/ThrowableTest.groovy
+++ src/test/java/com/simonalong/mikilin/throwable/ThrowableTest.groovy
@@ -3,8 +3,11 @@ package com.simonalong.mikilin.throwable
 import com.simonalong.mikilin.MkValidators
 import com.simonalong.mikilin.exception.MkCheckException
 import com.simonalong.mikilin.exception.MkException
+import com.simonalong.mikilin.parameter.ParameterFunOfChangeToService
 import org.junit.Assert
 import spock.lang.Specification
+
+import java.lang.reflect.Method
 
 /**
  * @author zhouzhenyong* @since 2019/3/10 下午10:16
@@ -34,5 +37,36 @@ class ThrowableTest extends Specification {
         name | result
         null | null
         "ok" | 1
+    }
+
+
+    def "异常的匹配转换"() {
+        given:
+        ParameterFunOfThrowableService service = new ParameterFunOfThrowableService()
+
+        when:
+        Exception ex = null
+        Method currentMethod = service.getClass().getMethod("funValue", String.class)
+
+        Object[] parameterValues = new Object[1]
+        parameterValues[0] = name
+        try {
+            MkValidators.check(currentMethod, parameterValues)
+        } catch (Exception e) {
+            ex = e
+        }
+
+        then:
+        if (null == result) {
+            ex instanceof TestException
+        } else {
+            Assert.assertEquals(null, ex)
+        }
+
+        where:
+        name   | result
+        "zhou" | null
+        "song" | null
+        "chen" | "ok"
     }
 }
