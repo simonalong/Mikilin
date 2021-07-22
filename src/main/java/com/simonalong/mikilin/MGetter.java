6e99b419418fefa983625c30a3017ef3aa513d07
shizi <lan.zhu.123@163.com>
2021-07-23 00:27:35 +0800

    add 添加编译时候的代码写入

---
+++ src/main/java/com/simonalong/mikilin/MGetter.java
@@ -0,0 +1,13 @@
+package com.simonalong.mikilin;
+
+import java.lang.annotation.*;
+
+/**
+ * @author shizi
+ * @since 2021-07-22 22:42:43
+ */
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.SOURCE)
+public @interface MGetter {
+
+}
