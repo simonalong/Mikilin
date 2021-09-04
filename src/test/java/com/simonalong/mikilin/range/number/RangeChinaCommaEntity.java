c93d321b3668fec59ac70d5321a1a303099776bc
shizi <lan.zhu.123@163.com>
2021-09-04 17:13:28 +0800

    add 添加range的中文逗号

---
+++ src/test/java/com/simonalong/mikilin/range/number/RangeChinaCommaEntity.java
@@ -0,0 +1,29 @@
+package com.simonalong.mikilin.range.number;
+
+import com.simonalong.mikilin.annotation.Matcher;
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+/**
+ * @author shizi
+ * @since 2020/1/3 10:42 下午
+ */
+@Data
+@Accessors(chain = true)
+public class RangeChinaCommaEntity {
+
+    /**
+     * 属性为大于等于100
+     */
+    @Matcher(range = "(100， )")
+    private Integer num1;
+
+    @Matcher(range = "[100，)")
+    private Integer num2;
+
+    @Matcher(range = "(， 50)")
+    private Integer num3;
+
+    @Matcher(range = "(，50]")
+    private Integer num4;
+}
